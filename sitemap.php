10ebece5f4ae0b8f7aaf9dc76f8506ab2344f5ca
AndyPilo <pilotoandy30@gmail.com>
2025-09-12 11:27:30 -0400

    cambio en el sitemap.php (2da vez)

--- sitemap.php
+++ sitemap.php
@@ -27,6 +27,25 @@ echo '<url>
         <priority>1.0</priority>
       </url>';
 
+echo '<url>
+        <loc>https://cuvarents.com/rents/casas-de-lujo</loc>
+        <priority>1.0</priority>
+      </url>';
+
+echo '<url>
+        <loc>https://cuvarents.com/rents/Casas-en-la-playa</loc> 
+        <priority>1.0</priority>
+      </url>';
+
+echo '<url>
+        <loc>https://cuvarents.com/rents/Casas-y-Apartamentos-por-largas-y-cortas-estancias</loc>     
+        <priority>1.0</priority>
+      </url>';
+
+echo '<url>
+        <loc>https://cuvarents.com/rents/Casas-y-Alojamientos-vacacionales</loc>
+        <priority>1.0</priority>
+      </url>';
 
 // Obtener todas las rentas
 $sql = "SELECT rental_id, rental_title FROM Rentals";
@@ -43,21 +62,5 @@ while ($row = $result->fetch_assoc()) {
             <priority>0.8</priority>
           </url>";
 }
-
-// Obtener todas las categorías
-$sqlCategories = "SELECT rental_category FROM Rentals";
-$resultCategories = $conn->query($sqlCategories);
-
-while ($row = $resultCategories->fetch_assoc()) {
-    $slug = slugify($row['rental_category']);
-    $url = "https://cuvarents.com/rents/" . $slug;
-
-    echo "<url>
-            <loc>$url</loc>
-            <priority>0.8</priority>
-          </url>";
-}
-
-
 
 echo '</urlset>';
